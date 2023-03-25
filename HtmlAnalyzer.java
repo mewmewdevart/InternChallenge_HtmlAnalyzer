9bda58c6657f03b9fefdf83241d41e81b5eb34e2
Meowbutu <mewmewdevart@gmail.com>
2023-03-24 23:23:55 -0300

    Estrutura basica do projeto em funcionamento : Implementação dos imports de URL, Scanner, MalFormed e IOExeption. O programa é capaz de ler todo o conteudo presente em uma pagina HTML

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -1,13 +1,40 @@
+/*
+• Author: Larissa Cristina Benedito ツ
+• Description: This program is a Html Analyzer for the Axur's technical challenge, capable of obtaining the text snippet
+from the deepest level of an HTML structure given a URL, even for poorly formed HTML structures.
+			Challenge Finished: 03/00/2023
+				Check out my other projects on Github: https://github.com/mewmewdevart
+*/
+import java.net.URL;
+import java.util.Scanner;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 public	class HtmlAnalyzer {
 	public static void main(String[] args) {
 		// Check if the correct number of arguments have been provided in the command line
 		if (args.length == 1) {
 			String urlString = args[0];
-			System.out.println(urlString);
+			try {
+				URL url = new URL(urlString);
+				System.out.println("URL: " + urlString);
+
+				// Scanner object to read the contents of the web page
+				Scanner scanner = new Scanner(url.openStream());
+				
+				// Read line by line from the web page and print it to the screen
+				while (scanner.hasNext()) {
+					System.out.println(scanner.nextLine());
+				}
+
+				scanner.close();
+			} catch (MalformedURLException e) {
+				System.out.println("malformed URL");
+			} catch (IOException e) {
+				System.out.println("URL connection error");
+			}
 		} else { 
-			System.out.println("Error: The program requires a limited arguments in the command line!");
-			System.out.println("Try usage: java HtmlAnalyzer <insert-url-here>");
+			System.out.println("Error: The program requires one argument in the command line!\n Try usage: java HtmlAnalyzer <insert-url-here>");
 		}
 	}
 }
